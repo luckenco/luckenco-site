c81ee85ce1f7a95620c24e59e69c7e7916845860
calbch <calbch@protonmail.com>
2024-08-23 12:42:56 +0200

    add cloudflare hosting plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://calbch.com",
+  output: "server",
+  adapter: cloudflare()
 });
