db8d4da162bfcd8d822dbed83a72f064ff24ac54
calbch <calbch@protonmail.com>
2024-11-05 09:59:29 +0100

    add blog post collection

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**\/*.md", base: "./posts" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishingDate: z.date(),
+    updateDate: z.date().optional(),
+    published: z.boolean()
+  })
+});
+
+export const collections = { blog };
