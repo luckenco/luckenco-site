d7f1fb511f6796fe461c3cefe1c42d568c7c47b0
calbch <calbch@protonmail.com>
2024-12-09 21:36:24 +0100

    remove cloudflare ssr plugin
    simply use wrangler to deploy a static page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://calbch.com",
-  output: "server",
-  adapter: cloudflare(),
 });
