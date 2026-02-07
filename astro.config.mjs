2e566a8e356ccf86591ae3ae74b3fb82ab27d846
Constantin Luckenbach <cluckenbach@protonmail.com>
2026-02-07 16:53:44 +0100

    chore: update domain to luckenbach.io and social handles to luckenco
    - Update site URL from luckenco.com to luckenbach.io
    - Update GitHub handle to luckenco
    - Update X/Twitter handle to luckenco
    - Add Instagram and LinkedIn social links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://luckenco.com",
+  site: "https://luckenbach.io",
   integrations: [sitemap()],
 });
